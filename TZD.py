a4b2c5f13e662adaa305c787aef53c06176ba15f
AaronGe88 <noahge88@163.com>
2015-11-24 21:25:49 +0800

    0.3

--- TZD.py
+++ TZD.py
@@ -5,6 +5,7 @@ from threading import Thread
 from queue import Queue
 import serial
 import ui_TZD
+import numpy as np
 class TZDUIWIDGET (QDialog, ui_TZD.Ui_Form):
 	def __init__(self, parent=None):
 		super(TZDUIWIDGET,self).__init__(parent)
@@ -26,25 +27,36 @@ class TZDUIWIDGET (QDialog, ui_TZD.Ui_Form):
 		self.timer.timeout.connect(self.onTimer)
 		self.btnReceive.clicked.connect(self.begin)
 		self.btnEnd.clicked.connect(self.endHandler)
-
-	
+		#test 
+		self.index = 0
 	def begin(self):
-		self.timer.start(500)
-		self.running = True
-		thread1 = Thread(target = self.read_from_serial,args=(self.byte_queue,))
-		thread1.start()
-		thread2 = Thread(target = self.find_effective_data,args=(self.byte_queue,self.results,))
-		thread2.start()
-		thread3 = Thread(target = self.data_process,args=(self.results,self.adcs))
-		thread3.start()
+		
+		
+		self.timer.start(50)
+		# self.running = True
+		# thread1 = Thread(target = self.read_from_serial,args=(self.byte_queue,))
+		# thread1.start()
+		# thread2 = Thread(target = self.find_effective_data,args=(self.byte_queue,self.results,))
+		# thread2.start()
+		# thread3 = Thread(target = self.data_process,args=(self.results,self.adcs))
+		# thread3.start()
 	def endHandler(self):
 		self.timer.stop()
-		self.ser.close()
+		#self.ser.close()
 	
 	def onTimer(self):
-		size = self.adcs.qsize()
-		for i in range(0,size):
-			self.adcs.get()
+		# size = self.adcs.qsize()
+		# for i in range(0,size):
+			# self.adcs.get()
+		self.index += 1
+		line1 = []
+		line2 = []
+		line3 = []
+		for ii in range(self.index):
+			line1.append(ii * 2 + np.random.randn())
+			line2.append(ii * 3 + np.random.randn())
+			line3.append(ii * 4 + np.random.randn())
+		self.widget.set_lines(line1,line2,line3)
 			
 		
 	def read_from_serial(self,out_q):
@@ -58,13 +70,13 @@ class TZDUIWIDGET (QDialog, ui_TZD.Ui_Form):
 			data = in_q.get()
 			if data is not 0xFE:
 				continue
-			effective.add(data)
+			effective.append(data)
 			data = in_q.get()
 			if data is not 0x0C:
 				continue
 			for _ in range(0,40):
 				data = in_q.get()
-				effective.add(data)
+				effective.append(data)
 			if effective[-1] is not 0xFF:
 				continue
 			out_q2.put(effective)
@@ -99,7 +111,7 @@ class TZDUIWIDGET (QDialog, ui_TZD.Ui_Form):
 			i32 = i32 + data[j+1];
 			i32 = i32 << 8
 			i32 = i32 + data[j];
-			adcs.add(i32 / 8)
+			adcs.append(i32 / 8)
 		out_q3.put(adcs)
 		in_q2.task_done()
 			
--- matplotlibwidget.py
+++ matplotlibwidget.py
@@ -7,21 +7,49 @@ import matplotlib.pyplot as plt
 class MplCanvas(FigureCanvas):
 	def __init__(self):
 		self.fig = plt.figure()
-		self.flatax = self.fig.add_axes([0,0,10,1])
+		self.flatax = self.fig.add_axes([0.1,0.1,1,1])
 		self.flatax.set_xlabel("time")
 		self.flatax.set_ylabel("flatness")
+		self.flatax.grid(True)
+		self.flatax.set_ylim([0, 200])  
+		self.flatax.set_xlim([0, 200]) 
 		FigureCanvas.__init__(self, self.fig)
 		FigureCanvas.setSizePolicy(self, QSizePolicy.Expanding,QSizePolicy.Expanding)
 		FigureCanvas.updateGeometry(self)
-		self.setlines([0],[0],[0])
-	def setlines(self,*args):
+		
+		self.flat0, = self.flatax.plot([0],[0],'r--')
+		self.flat1, = self.flatax.plot([0],[0],'gs')
+		self.flat2, = self.flatax.plot([0],[0],'bx')
+		self.draw()
+		
+	def set_lines(self,*args):
 		if len(args) is 3:
-			self.flatax.plot(args[0],'r--',args[1],'gs',args[2],'bx')
+			length = len(args[0])
+			self.flat0.set_xdata(range(length))
+			self.flat0.set_ydata(args[0])
+			self.flat1.set_xdata(range(length))
+			self.flat1.set_ydata(args[1])
+			self.flat2.set_xdata(range(length))
+			self.flat2.set_ydata(args[2])
+			max0 = max(args[0])
+			max1 = max(args[1])
+			max2 = max(args[2])
+			maxx = max([max0,max1,max2])
+			xliml,xlimu = self.flatax.get_xlim()
+			yliml,ylimu = self.flatax.get_ylim()
+			if xlimu < length:
+				self.flatax.set_xlim([0,length * 1.5])
+			if ylimu < maxx:
+				self.flatax.set_ylim([0,maxx * 1.5])
+			#self.flatax.draw_artist(self.flat0)
+			self.flatax.draw_artist(self.flat1)
+			self.flatax.draw_artist(self.flat2)
+			
+			self.flatax.autoscale_view()
 			self.draw()
 		else:
 			raise ValueError("Wrong arguements")
 		
-
 
 class MatplotlibWidget(QWidget):
 	def __init__(self, parent = None):
@@ -30,4 +58,5 @@ class MatplotlibWidget(QWidget):
 		self.vbl = QVBoxLayout()
 		self.vbl.addWidget(self.canvas)
 		self.setLayout(self.vbl)
-		
+	def set_lines(self,*args):
+		self.canvas.set_lines(*args)
